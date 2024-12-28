f61c871fea6b416cdeaa623327ff697841d7a7b1
ivanugarte <72155374+ivanugarte@users.noreply.github.com>
2024-12-28 21:01:54 +0000

    ia

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,172 +1,345 @@
-import datetime
-import random
-
-import altair as alt
-import numpy as np
-import pandas as pd
+import openai # type: ignore
 import streamlit as st
+import logging
+from PIL import Image, ImageEnhance
+import time
+import json
+import requests
+import base64
+from openai import OpenAI, OpenAIError # type: ignore
 
-# Show app title and description.
-st.set_page_config(page_title="Soporte de tickets", page_icon="🎫")
-st.title("🎫 Soporte de tickets")
-st.write(
-    """
-    This app shows how you can build an internal tool in Streamlit. Here, we are 
-    implementing a support ticket workflow. The user can create a ticket, edit 
-    existing tickets, and view some statistics.
-    """
-)
+# Configure logging
+logging.basicConfig(level=logging.INFO)
 
-# Create a random Pandas dataframe with existing tickets.
-if "df" not in st.session_state:
+# Constants
+NUMBER_OF_MESSAGES_TO_DISPLAY = 20
+API_DOCS_URL = "https://docs.streamlit.io/library/api-reference"
 
-    # Set seed for reproducibility.
-    np.random.seed(42)
+# Retrieve and validate API key
+OPENAI_API_KEY = st.secrets.get("OPENAI_API_KEY", None)
+if not OPENAI_API_KEY:
+    st.error("Please add your OpenAI API key to the Streamlit secrets.toml file.")
+    st.stop()
 
-    # Make up some fake issue descriptions.
-    issue_descriptions = [
-        "Network connectivity issues in the office",
-        "Software application crashing on startup",
-        "Printer not responding to print commands",
-        "Email server downtime",
-        "Data backup failure",
-        "Login authentication problems",
-        "Website performance degradation",
-        "Security vulnerability identified",
-        "Hardware malfunction in the server room",
-        "Employee unable to access shared files",
-        "Database connection failure",
-        "Mobile application not syncing data",
-        "VoIP phone system issues",
-        "VPN connection problems for remote employees",
-        "System updates causing compatibility issues",
-        "File server running out of storage space",
-        "Intrusion detection system alerts",
-        "Inventory management system errors",
-        "Customer data not loading in CRM",
-        "Collaboration tool not sending notifications",
-    ]
+# Assign OpenAI API Key
+openai.api_key = OPENAI_API_KEY
+client = openai.OpenAI()
 
-    # Generate the dataframe with 100 rows/tickets.
-    data = {
-        "ID": [f"TICKET-{i}" for i in range(1100, 1000, -1)],
-        "Issue": np.random.choice(issue_descriptions, size=100),
-        "Status": np.random.choice(["Open", "In Progress", "Closed"], size=100),
-        "Priority": np.random.choice(["High", "Medium", "Low"], size=100),
-        "Date Submitted": [
-            datetime.date(2023, 6, 1) + datetime.timedelta(days=random.randint(0, 182))
-            for _ in range(100)
-        ],
+# Streamlit Page Configuration
+st.set_page_config(
+    page_title="Streamly - An Intelligent Streamlit Assistant",
+    page_icon="imgs/avatar_streamly.png",
+    layout="wide",
+    initial_sidebar_state="auto",
+    menu_items={
+        "Get help": "https://github.com/AdieLaine/Streamly",
+        "Report a bug": "https://github.com/AdieLaine/Streamly",
+        "About": """
+            ## Streamly Streamlit Assistant
+            ### Powered using GPT-4o-mini
+
+            **GitHub**: https://github.com/AdieLaine/
+
+            The AI Assistant named, Streamly, aims to provide the latest updates from Streamlit,
+            generate code snippets for Streamlit widgets,
+            and answer questions about Streamlit's latest features, issues, and more.
+            Streamly has been trained on the latest Streamlit updates and documentation.
+        """
     }
-    df = pd.DataFrame(data)
-
-    # Save the dataframe in session state (a dictionary-like object that persists across
-    # page runs). This ensures our data is persisted when the app updates.
-    st.session_state.df = df
-
-
-# Show a section to add a new ticket.
-st.header("Adicionar ticket")
-
-# We're adding tickets via an `st.form` and some input widgets. If widgets are used
-# in a form, the app will only rerun once the submit button is pressed.
-with st.form("add_ticket_form"):
-    issue = st.text_area("Describa el problema")
-    priority = st.selectbox("Prioridad", ["Alta", "Mediana", "Baja"])
-    submitted = st.form_submit_button("Submit")
-
-if submitted:
-    # Make a dataframe for the new ticket and append it to the dataframe in session
-    # state.
-    recent_ticket_number = int(max(st.session_state.df.ID).split("-")[1])
-    today = datetime.datetime.now().strftime("%m-%d-%Y")
-    df_new = pd.DataFrame(
-        [
-            {
-                "ID": f"TICKET-{recent_ticket_number+1}",
-                "Issue": issue,
-                "Status": "Open",
-                "Priority": priority,
-                "Date Submitted": today,
-            }
-        ]
-    )
-
-    # Show a little success message.
-    st.write("Ticket submitted! Here are the ticket details:")
-    st.dataframe(df_new, use_container_width=True, hide_index=True)
-    st.session_state.df = pd.concat([df_new, st.session_state.df], axis=0)
-
-# Show section to view and edit existing tickets in a table.
-st.header("Existing tickets")
-st.write(f"Number of tickets: `{len(st.session_state.df)}`")
-
-st.info(
-    "You can edit the tickets by double clicking on a cell. Note how the plots below "
-    "update automatically! You can also sort the table by clicking on the column headers.",
-    icon="✍️",
 )
 
-# Show the tickets dataframe with `st.data_editor`. This lets the user edit the table
-# cells. The edited data is returned as a new dataframe.
-edited_df = st.data_editor(
-    st.session_state.df,
-    use_container_width=True,
-    hide_index=True,
-    column_config={
-        "Status": st.column_config.SelectboxColumn(
-            "Status",
-            help="Ticket status",
-            options=["Open", "In Progress", "Closed"],
-            required=True,
-        ),
-        "Priority": st.column_config.SelectboxColumn(
-            "Priority",
-            help="Priority",
-            options=["High", "Medium", "Low"],
-            required=True,
-        ),
-    },
-    # Disable editing the ID and Date Submitted columns.
-    disabled=["ID", "Date Submitted"],
-)
+# Streamlit Title
+st.title("Streamly Streamlit Assistant")
 
-# Show some metrics and charts about the ticket.
-st.header("Statistics")
+def img_to_base64(image_path):
+    """Convert image to base64."""
+    try:
+        with open(image_path, "rb") as img_file:
+            return base64.b64encode(img_file.read()).decode()
+    except Exception as e:
+        logging.error(f"Error converting image to base64: {str(e)}")
+        return None
 
-# Show metrics side by side using `st.columns` and `st.metric`.
-col1, col2, col3 = st.columns(3)
-num_open_tickets = len(st.session_state.df[st.session_state.df.Status == "Open"])
-col1.metric(label="Number of open tickets", value=num_open_tickets, delta=10)
-col2.metric(label="First response time (hours)", value=5.2, delta=-1.5)
-col3.metric(label="Average resolution time (hours)", value=16, delta=2)
+@st.cache_data(show_spinner=False)
+def long_running_task(duration):
+    """
+    Simulates a long-running operation.
 
-# Show two Altair charts using `st.altair_chart`.
-st.write("")
-st.write("##### Ticket status per month")
-status_plot = (
-    alt.Chart(edited_df)
-    .mark_bar()
-    .encode(
-        x="month(Date Submitted):O",
-        y="count():Q",
-        xOffset="Status:N",
-        color="Status:N",
+    Parameters:
+    - duration: int, duration of the task in seconds
+
+    Returns:
+    - str: Completion message
+    """
+    time.sleep(duration)
+    return "Long-running operation completed."
+
+@st.cache_data(show_spinner=False)
+def load_and_enhance_image(image_path, enhance=False):
+    """
+    Load and optionally enhance an image.
+
+    Parameters:
+    - image_path: str, path of the image
+    - enhance: bool, whether to enhance the image or not
+
+    Returns:
+    - img: PIL.Image.Image, (enhanced) image
+    """
+    img = Image.open(image_path)
+    if enhance:
+        enhancer = ImageEnhance.Contrast(img)
+        img = enhancer.enhance(1.8)
+    return img
+
+@st.cache_data(show_spinner=False)
+def load_streamlit_updates():
+    """Load the latest Streamlit updates from a local JSON file."""
+    try:
+        with open("data/streamlit_updates.json", "r") as f:
+            return json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError) as e:
+        logging.error(f"Error loading JSON: {str(e)}")
+        return {}
+
+def get_streamlit_api_code_version():
+    """
+    Get the current Streamlit API code version from the Streamlit API documentation.
+
+    Returns:
+    - str: The current Streamlit API code version.
+    """
+    try:
+        response = requests.get(API_DOCS_URL)
+        if response.status_code == 200:
+            return "1.36"
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Error connecting to the Streamlit API documentation: {str(e)}")
+    return None
+
+def display_streamlit_updates():
+    """Display the latest updates of the Streamlit."""
+    with st.expander("Streamlit 1.36 Announcement", expanded=False):
+        st.markdown("For more details on this version, check out the [Streamlit Forum post](https://docs.streamlit.io/library/changelog#version).")
+
+def initialize_conversation():
+    """
+    Initialize the conversation history with system and assistant messages.
+
+    Returns:
+    - list: Initialized conversation history.
+    """
+    assistant_message = "Hello! I am Streamly. How can I assist you with Streamlit today?"
+
+    conversation_history = [
+        {"role": "system", "content": "You are Streamly, a specialized AI assistant trained in Streamlit."},
+        {"role": "system", "content": "Streamly, is powered by the OpenAI GPT-4o-mini model, released on July 18, 2024."},
+        {"role": "system", "content": "You are trained up to Streamlit Version 1.36.0, release on June 20, 2024."},
+        {"role": "system", "content": "Refer to conversation history to provide context to your response."},
+        {"role": "system", "content": "You were created by Madie Laine, an OpenAI Researcher."},
+        {"role": "assistant", "content": assistant_message}
+    ]
+    return conversation_history
+
+@st.cache_data(show_spinner=False)
+def get_latest_update_from_json(keyword, latest_updates):
+    """
+    Fetch the latest Streamlit update based on a keyword.
+
+    Parameters:
+    - keyword (str): The keyword to search for in the Streamlit updates.
+    - latest_updates (dict): The latest Streamlit updates data.
+
+    Returns:
+    - str: The latest update related to the keyword, or a message if no update is found.
+    """
+    for section in ["Highlights", "Notable Changes", "Other Changes"]:
+        for sub_key, sub_value in latest_updates.get(section, {}).items():
+            for key, value in sub_value.items():
+                if keyword.lower() in key.lower() or keyword.lower() in value.lower():
+                    return f"Section: {section}\nSub-Category: {sub_key}\n{key}: {value}"
+    return "No updates found for the specified keyword."
+
+def construct_formatted_message(latest_updates):
+    """
+    Construct formatted message for the latest updates.
+
+    Parameters:
+    - latest_updates (dict): The latest Streamlit updates data.
+
+    Returns:
+    - str: Formatted update messages.
+    """
+    formatted_message = []
+    highlights = latest_updates.get("Highlights", {})
+    version_info = highlights.get("Version 1.36", {})
+    if version_info:
+        description = version_info.get("Description", "No description available.")
+        formatted_message.append(f"- **Version 1.36**: {description}")
+
+    for category, updates in latest_updates.items():
+        formatted_message.append(f"**{category}**:")
+        for sub_key, sub_values in updates.items():
+            if sub_key != "Version 1.36":  # Skip the version info as it's already included
+                description = sub_values.get("Description", "No description available.")
+                documentation = sub_values.get("Documentation", "No documentation available.")
+                formatted_message.append(f"- **{sub_key}**: {description}")
+                formatted_message.append(f"  - **Documentation**: {documentation}")
+    return "\n".join(formatted_message)
+
+@st.cache_data(show_spinner=False)
+def on_chat_submit(chat_input, latest_updates):
+    """
+    Handle chat input submissions and interact with the OpenAI API.
+
+    Parameters:
+    - chat_input (str): The chat input from the user.
+    - latest_updates (dict): The latest Streamlit updates fetched from a JSON file or API.
+
+    Returns:
+    - None: Updates the chat history in Streamlit's session state.
+    """
+    user_input = chat_input.strip().lower()
+
+    if 'conversation_history' not in st.session_state:
+        st.session_state.conversation_history = initialize_conversation()
+
+    st.session_state.conversation_history.append({"role": "user", "content": user_input})
+
+    try:
+        model_engine = "gpt-4o-mini"
+        assistant_reply = ""
+
+        if "latest updates" in user_input:
+            assistant_reply = "Here are the latest highlights from Streamlit:\n"
+            highlights = latest_updates.get("Highlights", {})
+            if highlights:
+                for version, info in highlights.items():
+                    description = info.get("Description", "No description available.")
+                    assistant_reply += f"- **{version}**: {description}\n"
+            else:
+                assistant_reply = "No highlights found."
+        else:
+            response = client.chat.completions.create(
+                model=model_engine,
+                messages=st.session_state.conversation_history
+            )
+            assistant_reply = response.choices[0].message.content
+
+        st.session_state.conversation_history.append({"role": "assistant", "content": assistant_reply})
+        st.session_state.history.append({"role": "user", "content": user_input})
+        st.session_state.history.append({"role": "assistant", "content": assistant_reply})
+
+    except OpenAIError as e:
+        logging.error(f"Error occurred: {e}")
+        st.error(f"OpenAI Error: {str(e)}")
+
+def initialize_session_state():
+    """Initialize session state variables."""
+    if "history" not in st.session_state:
+        st.session_state.history = []
+    if 'conversation_history' not in st.session_state:
+        st.session_state.conversation_history = []
+
+def main():
+    """
+    Display Streamlit updates and handle the chat interface.
+    """
+    initialize_session_state()
+
+    if not st.session_state.history:
+        initial_bot_message = "Hello! How can I assist you with Streamlit today?"
+        st.session_state.history.append({"role": "assistant", "content": initial_bot_message})
+        st.session_state.conversation_history = initialize_conversation()
+
+    # Insert custom CSS for glowing effect
+    st.markdown(
+        """
+        <style>
+        .cover-glow {
+            width: 100%;
+            height: auto;
+            padding: 3px;
+            box-shadow: 
+                0 0 5px #330000,
+                0 0 10px #660000,
+                0 0 15px #990000,
+                0 0 20px #CC0000,
+                0 0 25px #FF0000,
+                0 0 30px #FF3333,
+                0 0 35px #FF6666;
+            position: relative;
+            z-index: -1;
+            border-radius: 45px;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True,
     )
-    .configure_legend(
-        orient="bottom", titleFontSize=14, labelFontSize=14, titlePadding=5
-    )
-)
-st.altair_chart(status_plot, use_container_width=True, theme="streamlit")
 
-st.write("##### Current ticket priorities")
-priority_plot = (
-    alt.Chart(edited_df)
-    .mark_arc()
-    .encode(theta="count():Q", color="Priority:N")
-    .properties(height=300)
-    .configure_legend(
-        orient="bottom", titleFontSize=14, labelFontSize=14, titlePadding=5
-    )
-)
-st.altair_chart(priority_plot, use_container_width=True, theme="streamlit")
+    # Load and display sidebar image
+    img_path = "imgs/sidebar_streamly_avatar.png"
+    img_base64 = img_to_base64(img_path)
+    if img_base64:
+        st.sidebar.markdown(
+            f'<img src="data:image/png;base64,{img_base64}" class="cover-glow">',
+            unsafe_allow_html=True,
+        )
+
+    st.sidebar.markdown("---")
+
+    # Sidebar for Mode Selection
+    mode = st.sidebar.radio("Select Mode:", options=["Latest Updates", "Chat with Streamly"], index=1)
+
+    st.sidebar.markdown("---")
+
+    # Display basic interactions
+    show_basic_info = st.sidebar.checkbox("Show Basic Interactions", value=True)
+    if show_basic_info:
+        st.sidebar.markdown("""
+        ### Basic Interactions
+        - **Ask About Streamlit**: Type your questions about Streamlit's latest updates, features, or issues.
+        - **Search for Code**: Use keywords like 'code example', 'syntax', or 'how-to' to get relevant code snippets.
+        - **Navigate Updates**: Switch to 'Updates' mode to browse the latest Streamlit updates in detail.
+        """)
+
+    # Display advanced interactions
+    show_advanced_info = st.sidebar.checkbox("Show Advanced Interactions", value=False)
+    if show_advanced_info:
+        st.sidebar.markdown("""
+        ### Advanced Interactions
+        - **Generate an App**: Use keywords like **generate app**, **create app** to get a basic Streamlit app code.
+        - **Code Explanation**: Ask for **code explanation**, **walk me through the code** to understand the underlying logic of Streamlit code snippets.
+        - **Project Analysis**: Use **analyze my project**, **technical feedback** to get insights and recommendations on your current Streamlit project.
+        - **Debug Assistance**: Use **debug this**, **fix this error** to get help with troubleshooting issues in your Streamlit app.
+        """)
+
+    st.sidebar.markdown("---")
+
+    # Load and display image with glowing effect
+    img_path = "imgs/stsidebarimg.png"
+    img_base64 = img_to_base64(img_path)
+    if img_base64:
+        st.sidebar.markdown(
+            f'<img src="data:image/png;base64,{img_base64}" class="cover-glow">',
+            unsafe_allow_html=True,
+        )
+
+    if mode == "Chat with Streamly":
+        chat_input = st.chat_input("Ask me about Streamlit updates:")
+        if chat_input:
+            latest_updates = load_streamlit_updates()
+            on_chat_submit(chat_input, latest_updates)
+
+        # Display chat history
+        for message in st.session_state.history[-NUMBER_OF_MESSAGES_TO_DISPLAY:]:
+            role = message["role"]
+            avatar_image = "imgs/avatar_streamly.png" if role == "assistant" else "imgs/stuser.png" if role == "user" else None
+            with st.chat_message(role, avatar=avatar_image):
+                st.write(message["content"])
+
+    else:
+        display_streamlit_updates()
+
+if __name__ == "__main__":
+    main()
